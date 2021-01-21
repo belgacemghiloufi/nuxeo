6fa896776f1ec336599b08dbb850c2c74c551d98
Kevin Leturc <kleturc@nuxeo.com>
2021-01-21 12:00:11 +0100

    NXP-29920: Extract InstanceInfo construction

--- modules/runtime/nuxeo-launcher-commons/src/main/java/org/nuxeo/launcher/info/InstanceInfo.java
+++ modules/runtime/nuxeo-launcher-commons/src/main/java/org/nuxeo/launcher/info/InstanceInfo.java
@@ -19,14 +19,31 @@
 
 package org.nuxeo.launcher.info;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.nuxeo.launcher.config.ConfigurationGenerator.NUXEO_PROFILES;
+import static org.nuxeo.launcher.config.ConfigurationGenerator.TEMPLATE_SEPARATOR;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import org.nuxeo.common.Environment;
+import org.nuxeo.common.codec.Crypto;
+import org.nuxeo.connect.update.LocalPackage;
+import org.nuxeo.launcher.config.ConfigurationGenerator;
+import org.nuxeo.launcher.config.ConfigurationHolder;
 
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name = "instance")
@@ -54,4 +71,74 @@ public class InstanceInfo {
     @XmlElement(name = "configuration")
     public ConfigurationInfo config;
 
+    /**
+     * Introspect the server and builds the instance info.
+     *
+     * @since 11.5
+     */
+    public static InstanceInfo from(ConfigurationHolder configHolder, String clid, List<LocalPackage> pkgs) {
+        InstanceInfo nxInstance = new InstanceInfo();
+        nxInstance.NUXEO_CONF = configHolder.getNuxeoConfPath().toString();
+        nxInstance.NUXEO_HOME = configHolder.getHomePath().toString();
+        // distribution
+        Path distFile = configHolder.getConfigurationPath().resolve("distribution.properties");
+        if (Files.notExists(distFile)) {
+            // fallback in the file in templates
+            distFile = configHolder.getTemplatesPath().resolve(Path.of("common", "config", "distribution.properties"));
+        }
+        try {
+            nxInstance.distribution = new DistributionInfo(distFile.toFile());
+        } catch (IOException e) {
+            nxInstance.distribution = new DistributionInfo();
+        }
+        // packages
+        nxInstance.clid = clid;
+        Set<String> pkgTemplates = new HashSet<>();
+        for (LocalPackage pkg : pkgs) {
+            final PackageInfo info = new PackageInfo(pkg);
+            nxInstance.packages.add(info);
+            pkgTemplates.addAll(info.templates);
+        }
+        nxInstance.config = new ConfigurationInfo();
+        // profiles
+        String profiles = System.getenv(NUXEO_PROFILES);
+        if (isNotBlank(profiles)) {
+            nxInstance.config.profiles.addAll(Arrays.asList(profiles.split(TEMPLATE_SEPARATOR)));
+        }
+        // templates
+        nxInstance.config.dbtemplate = configHolder.getIncludedDBTemplateName();
+        List<String> userTemplates = configHolder.getIncludedTemplateNames();
+        for (String template : userTemplates) {
+            if (template.equals(nxInstance.config.dbtemplate)) {
+                continue;
+            }
+            if (pkgTemplates.contains(template)) {
+                nxInstance.config.pkgtemplates.add(template);
+            } else {
+                if (Files.exists(configHolder.getTemplatesPath().resolve(template))) {
+                    nxInstance.config.basetemplates.add(template);
+                } else {
+                    nxInstance.config.usertemplates.add(template);
+                }
+            }
+        }
+        // Settings from nuxeo.conf
+        nxInstance.config.keyvals = computeKeyVals(configHolder, configHolder.keySet());
+        // Effective configuration for environment and profiles
+        nxInstance.config.allkeyvals = computeKeyVals(configHolder, configHolder.stringPropertyNames());
+        return nxInstance;
+    }
+
+    protected static List<KeyValueInfo> computeKeyVals(ConfigurationHolder configHolder, Set<String> keys) {
+        var keyVals = new ArrayList<KeyValueInfo>(keys.size());
+        for (String key : new TreeSet<>(keys)) {
+            String value = configHolder.getRawProperty(key);
+            if (ConfigurationGenerator.SECRET_KEYS.contains(key) || key.contains("password")
+                    || key.equals(Environment.SERVER_STATUS_KEY) || Crypto.isEncrypted(value)) {
+                value = "********";
+            }
+            keyVals.add(new KeyValueInfo(key, value));
+        }
+        return keyVals;
+    }
 }
